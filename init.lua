091019e89ed42b169f7e31c8a0b1b6a3a4bf9fda
scowan1995 <7226103+scowan1995@users.noreply.github.com>
2024-05-25 17:22:30 +0100

    merge conflict

--- init.lua
+++ init.lua
@@ -91,13 +91,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-<<<<<<< Updated upstream
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-=======
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
->>>>>>> Stashed changes
+
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
